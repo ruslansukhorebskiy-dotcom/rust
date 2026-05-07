49255e51dd4b5479055beb9901f12e873e94846a
Ruslan <ruslan.sukhorebskiy@e-u.edu.ua>
2026-05-07 15:39:59 +0300

    Rust practice 6

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,22 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        return "NO".to_string();
+    }
+
+    if (x2 - x1) % (v1 - v2) == 0 {
+        "YES".to_string()
+    } else {
+        "NO".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kangaroo_yes() {
+        assert_eq!(kangaroo(0, 3, 4, 2), "YES");
+    }
+}
