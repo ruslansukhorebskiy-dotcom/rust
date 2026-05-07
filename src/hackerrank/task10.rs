c3d2eeb958550aae01d61800e89b6fc6d1660226
Ruslan <ruslan.sukhorebskiy@e-u.edu.ua>
2026-05-07 15:59:22 +0300

    Practice 10 sock merchant

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,20 @@
-// https://www.hackerrank.com/challenges/sock-merchant/problem
+pub fn sock_merchant(ar: &[i32]) -> i32 {
+    let mut counts = [0; 101];
+
+    for &sock in ar {
+        counts[sock as usize] += 1;
+    }
+
+    counts.iter().map(|count| count / 2).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sock_merchant() {
+        let socks = vec![10, 20, 20, 10, 10, 30, 50, 10, 20];
+        assert_eq!(sock_merchant(&socks), 3);
+    }
+}
