30e642b5b208fbc675c5b192d97f355abc9f05d3
Ruslan <ruslan.sukhorebskiy@e-u.edu.ua>
2026-05-07 15:45:33 +0300

    Rust practice 8

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,29 @@
-// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+pub fn breaking_records(scores: &[i32]) -> Vec<i32> {
+    let mut highest = scores[0];
+    let mut lowest = scores[0];
+    let mut highest_count = 0;
+    let mut lowest_count = 0;
+
+    for &score in scores.iter().skip(1) {
+        if score > highest {
+            highest = score;
+            highest_count += 1;
+        } else if score < lowest {
+            lowest = score;
+            lowest_count += 1;
+        }
+    }
+
+    vec![highest_count, lowest_count]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_breaking_records() {
+        let scores = vec![10, 5, 20, 20, 4, 5, 2, 25, 1];
+        assert_eq!(breaking_records(&scores), vec![2, 4]);
+    }
+}
