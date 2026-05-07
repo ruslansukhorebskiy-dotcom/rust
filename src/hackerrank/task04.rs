5a58ccb85a0cc3d67b47c7241b4e5fc3fe379596
Ruslan <ruslan.sukhorebskiy@e-u.edu.ua>
2026-05-07 15:30:23 +0300

    Added Practice 4

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,30 @@
 // https://www.hackerrank.com/challenges/grading/problem
+pub fn grading_students(grades: &[i32]) -> Vec<i32> {
+    grades
+        .iter()
+        .map(|&grade| {
+            if grade < 38 {
+                grade
+            } else {
+                let next_multiple = ((grade / 5) + 1) * 5;
+                if next_multiple - grade < 3 {
+                    next_multiple
+                } else {
+                    grade
+                }
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_grading_students() {
+        let grades = vec![73, 67, 38, 33];
+        let expected = vec![75, 67, 40, 33];
+        assert_eq!(grading_students(&grades), expected);
+    }
+}
--- src/hackerrank/task05.rs
+++ src/hackerrank/task05.rs
@@ -1,1 +1,41 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
+pub fn count_apples_and_oranges(
+    s: i32,
+    t: i32,
+    a: i32,
+    b: i32,
+    apples: &[i32],
+    oranges: &[i32],
+) -> (i32, i32) {
+    let apples_count = apples
+        .iter()
+        .filter(|&&distance| {
+            let position = a + distance;
+            position >= s && position <= t
+        })
+        .count() as i32;
+
+    let oranges_count = oranges
+        .iter()
+        .filter(|&&distance| {
+            let position = b + distance;
+            position >= s && position <= t
+        })
+        .count() as i32;
+
+    (apples_count, oranges_count)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_apples_and_oranges() {
+        let apples = vec![-2, 2, 1];
+        let oranges = vec![5, -6];
+        let expected = (1, 1);
+
+        assert_eq!(count_apples_and_oranges(7, 11, 5, 15, &apples, &oranges), expected);
+    }
+}
