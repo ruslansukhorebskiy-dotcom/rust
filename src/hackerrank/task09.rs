82f7418922cf18ead346d47171b1fc15911f1cdf
Ruslan <ruslan.sukhorebskiy@e-u.edu.ua>
2026-05-07 15:51:55 +0300

    Rust practice 9

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,28 @@
-// https://www.hackerrank.com/challenges/migratory-birds/problem
+pub fn migratory_birds(arr: &[i32]) -> i32 {
+    let mut counts = [0; 6];
+
+    for &bird in arr {
+        counts[bird as usize] += 1;
+    }
+
+    let mut result = 1;
+
+    for bird in 2..=5 {
+        if counts[bird] > counts[result] {
+            result = bird;
+        }
+    }
+
+    result as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_migratory_birds() {
+        let birds = vec![1, 4, 4, 4, 5, 3];
+        assert_eq!(migratory_birds(&birds), 4);
+    }
+}
