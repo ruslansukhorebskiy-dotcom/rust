e4c4dffbeb9543516f42b5a2ef745b93092a809b
Ruslan <ruslan.sukhorebskiy@e-u.edu.ua>
2026-05-07 15:08:15 +0300

    Added Practice 3 staircase

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,27 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+pub fn staircase(n: usize) -> String {
+    let mut result = String::new();
+
+    for i in 1..=n {
+        let spaces = " ".repeat(n - i);
+        let hashes = "#".repeat(i);
+        result.push_str(&(spaces + &hashes));
+
+        if i != n {
+            result.push('\n');
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_staircase() {
+        let expected = "   #\n  ##\n ###\n####";
+        assert_eq!(staircase(4), expected);
+    }
+}
